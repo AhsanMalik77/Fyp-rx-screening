a57152482bced99bf474be2b219bb770bcf25448
Ahsan Malik <116307709+AhsanMalik77@users.noreply.github.com>
2025-12-07 18:12:36 +0500

    login signup updated

--- Fyp-rx/Controllers/UserController.cs
+++ Fyp-rx/Controllers/UserController.cs
@@ -11,44 +11,65 @@ namespace Fyp_rx.Controllers
     public class UserController : ApiController
     {
         RX_SCREENINGEntities db = new RX_SCREENINGEntities();
+
+        //signup func
         [HttpPost]
         public HttpResponseMessage Signup(User obj)
         {
-            if (obj != null)
+            if (obj == null)
             {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Data");
+            }
+
+            try
+            {
+
+                var checkEmail = db.User.FirstOrDefault(u => u.Email == obj.Email);
+                if (checkEmail != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.Conflict, "Email already exists");
+                }
                 db.User.Add(obj);
                 db.SaveChanges();
-                return Request.CreateResponse(HttpStatusCode.OK, "Signup Successfull");
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Signup Successful");
             }
-            return Request.CreateResponse(HttpStatusCode.BadRequest, "Some Thing Wrong");
-        }
-        [HttpGet]
-        public HttpResponseMessage Signin(String email, String Password)
-        {
-            if (email != null && Password != null)
+            catch (Exception ex)
             {
 
-                var res = db.User.Where(u => u.Email == email && u.Password == Password).FirstOrDefault();
-                if (res == null)
-                {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Server Error: " + ex.Message);
+            }
+        }
 
-                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Password Or Email Is Incorrect.....");
-
-                }
-                else
-                {
-
-
-                    return Request.CreateErrorResponse(HttpStatusCode.OK, "Login SuccessFull...");
-
-
-                }
+        //login api
+        [HttpGet]
+        public HttpResponseMessage Signin(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Fields empty. Please enter data.");
             }
 
-            return Request.CreateResponse(HttpStatusCode.BadRequest, "Fields Empty Enter Data.........");
+            var user = db.User.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Password or Email is incorrect.");
+            }
 
 
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                Message = "Login Successful",
+                User = new
+                {
+                    user.UserId,
+                    user.Name
+                }
+            
+            });
         }
-    }
-}
 
+    }
+
+}
