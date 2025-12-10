909d4997072f0f62a70b75330b8cb601b8e8ac51
Ahsan Malik <116307709+AhsanMalik77@users.noreply.github.com>
2025-12-10 13:23:35 +0500

    User Profile and Edit User Profile Added

--- Fyp-rx/Controllers/UserController.cs
+++ Fyp-rx/Controllers/UserController.cs
@@ -45,7 +45,7 @@ namespace Fyp_rx.Controllers
         [HttpGet]
         public HttpResponseMessage Signin(string email, string password)
         {
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            if (email==null || password==null)
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, "Fields empty. Please enter data.");
             }
@@ -69,6 +69,110 @@ namespace Fyp_rx.Controllers
             
             });
         }
+
+        [HttpGet]
+        public HttpResponseMessage Profile(int id)
+        {
+            if (id<=0)
+            {
+                //id jo login ka time variable ma save ho ge front end ma
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "something wrong please put id from your saved variable");
+            }
+           
+                var Profile = db.User.FirstOrDefault(p => p.UserId==id);
+
+            if (Profile==null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Profile Not Found");
+            }
+
+            return (Request.CreateResponse(HttpStatusCode.OK,
+                        new
+                        {
+                            
+                            Profile.Name,
+                            Profile.Email,
+                            Profile.Contact,
+                            Profile.Address,
+                        }));
+ 
+
+        }
+        [HttpPut]
+        [Route("api/User/EditProfile")]
+        public HttpResponseMessage EditProfile(int id=0,int age=0,String email=null,String contact = null,String address = null)
+        {
+            if (id == 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Put Id");
+
+            }
+            if (age == 0 && string.IsNullOrEmpty(email) && string.IsNullOrEmpty(contact) && string.IsNullOrEmpty(address)){
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Atleast One Field is required to update");
+
+            }
+            var user = db.User.FirstOrDefault(u => u.UserId == id);
+            if (user == null)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Not Found User of this id");
+            }
+
+            bool isupdated = false;
+            if (age != 0)
+            {
+                if (age < 15 || age > 100)
+                {
+                    return Request.CreateResponse(HttpStatusCode.Conflict, "Age Must Be Between 15 to 100");
+                }
+                if (user.Age != age) { 
+                user.Age = age;
+                isupdated = true;
+
+                    }
+            }
+            if (!string.IsNullOrEmpty(email))
+            {
+                bool echeck = db.User.Any(u => u.Email == email&& user.UserId!=id);
+                if (echeck)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Email ALready Exist");
+                }
+                if (user.Email != email)
+                {
+                    user.Email = email;
+                    isupdated = true;
+                }
+
+            }
+
+                if (!string.IsNullOrEmpty(contact))
+                {
+                if (user.Contact != contact)
+                {
+                    user.Contact = contact;
+                    isupdated = true;
+                }
+                }
+                if (!string.IsNullOrEmpty(address))
+                {
+                if (user.Address != address)
+                {
+                    user.Address = address;
+                    isupdated = true;
+                }
+                }
+                if (isupdated)
+                {
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Profile Updated successfully");
+                }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "No Need To Update");
+
+            
+        }
+       
 
     }
 
